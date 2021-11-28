e71f32f5998e5c3d0fa30b416afa6d2a03b50043
VladisB <bykovvladislavv@gmail.com>
2021-11-28 16:59:05 +0200

    Roles module

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,6 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { BelongsToMany, Column, DataType, Model, Table } from 'sequelize-typescript';
+import { Role } from 'src/roles/roles.model';
+import { UserRoles } from 'src/roles/user-roles.model';
 
 interface UserCreationAttrs {
   email: string;
@@ -20,4 +22,7 @@ export class User extends Model<User, UserCreationAttrs> {
 
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[]
 }
