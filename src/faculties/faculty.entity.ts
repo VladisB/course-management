c53b605983c5c544725cedd645306bed292dbe95
sombra-bykov-vladyslav <vladyslav.bykov@sombrainc.com>
2023-02-06 21:47:16 +0200

    Faculty entity update

--- src/faculties/faculty.entity.ts
+++ src/faculties/faculty.entity.ts
@@ -1,4 +1,12 @@
-import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
 import { Group } from "../groups/group.entity";
 
 @Entity()
@@ -11,4 +19,14 @@ export class Faculty extends BaseEntity {
 
     @OneToMany(() => Group, (group) => group.faculty)
     groups: Group[];
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    public createdAt: Date;
+
+    @UpdateDateColumn({
+        type: "timestamp",
+        default: () => "CURRENT_TIMESTAMP(6)",
+        onUpdate: "CURRENT_TIMESTAMP(6)",
+    })
+    public updatedAt: Date;
 }
