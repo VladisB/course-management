1c4d4e4d8a04ce7a0c34d0f438a79873b5f65db9
sombra-bykov-vladyslav <vladyslav.bykov@sombrainc.com>
2023-02-12 21:12:16 +0200

    Query params DTO

---
+++ src/common/dto/query-params.dto.ts
@@ -0,0 +1,46 @@
+import { Transform, Type } from "class-transformer";
+import { IsEnum, IsNumber, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+// TODO: Move this to a separate file
+export enum SortDirection {
+    ASC = "ASC",
+    DESC = "DESC",
+}
+
+export class QueryParamsDTO {
+    @Type(() => Number)
+    @IsNumber()
+    @IsOptional()
+    page?: number;
+
+    @Type(() => Number)
+    @IsNumber()
+    @IsOptional()
+    limit?: number;
+
+    @IsString()
+    @MaxLength(25)
+    @MinLength(1)
+    @IsOptional()
+    search?: string;
+
+    @IsString()
+    @MaxLength(25)
+    @MinLength(1)
+    @IsOptional()
+    searchBy?: string;
+
+    @IsString()
+    @MaxLength(25)
+    @MinLength(1)
+    @IsOptional()
+    @Transform(({ value }) => value?.toUpperCase().trim())
+    @IsEnum(SortDirection)
+    sortType?: SortDirection;
+
+    @IsString()
+    @MaxLength(25)
+    @MinLength(1)
+    @IsOptional()
+    sortBy?: string;
+}
