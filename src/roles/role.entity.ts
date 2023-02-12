13c9cc6e2b3ad25e4e7326587d0c48a14d36e7b9
sombra-bykov-vladyslav <vladyslav.bykov@sombrainc.com>
2023-02-12 18:04:53 +0200

    update role entity

--- src/roles/role.entity.ts
+++ src/roles/role.entity.ts
@@ -20,13 +20,18 @@ export class Role extends BaseEntity {
     @OneToMany(() => User, (user) => user.role)
     users: User[];
 
-    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    @CreateDateColumn({
+        type: "timestamp",
+        default: () => "CURRENT_TIMESTAMP(6)",
+        name: "created_at",
+    })
     public createdAt: Date;
 
     @UpdateDateColumn({
         type: "timestamp",
         default: () => "CURRENT_TIMESTAMP(6)",
         onUpdate: "CURRENT_TIMESTAMP(6)",
+        name: "updated_at",
     })
     public updatedAt: Date;
 }
