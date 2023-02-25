8246f794667d6665d4e1ed3acd1aa7c53f43d0dc
sombra-bykov-vladyslav <vladyslav.bykov@sombrainc.com>
2023-02-25 19:52:44 +0200

    Updated shared logic

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { ConfigService } from "@nestjs/config";
-import { HttpExceptionFilter } from "./exception-filters/http-exception.filter";
+import { HttpExceptionFilter } from "./common/exception-filters/http-exception.filter";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
