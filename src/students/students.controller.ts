65558fe9a9c8cb048d297fb80cef6d04d344f1c6
Vladyslav Bykov <vladyslav.bykov@sombrainc.com>
2023-05-21 19:07:04 +0300

    Students module permissions

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -4,6 +4,7 @@ import {
     Param,
     ParseIntPipe,
     Query,
+    UseGuards,
     UsePipes,
     ValidationPipe,
 } from "@nestjs/common";
@@ -11,7 +12,14 @@ import { IStudentsService } from "./students.service";
 import { QueryParamsDTO } from "src/common/dto/query-params.dto";
 import { StudentDetailsViewModel, StudentListViewModel } from "./view-models";
 import { DataListResponse } from "src/common/db/data-list-response";
+import { RoleName } from "src/roles/roles.enum";
+import { AuthGuard } from "@nestjs/passport";
+import { Strategies } from "src/auth/strategies.enum";
+import { Roles } from "src/roles/roles-auth.decorator";
+import { RolesGuard } from "src/roles/roles.guard";
 
+@Roles(RoleName.Admin, RoleName.Instructor)
+@UseGuards(AuthGuard(Strategies.JWT), RolesGuard)
 @Controller("students")
 export class StudentsController {
     constructor(private readonly studentsService: IStudentsService) {}
