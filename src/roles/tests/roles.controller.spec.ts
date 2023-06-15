8e4a3f5fbd0dcb6ed8de63b3bfc3b643bd0e48fa
Vladyslav Bykov <vladyslav.bykov@sombrainc.com>
2023-06-15 14:41:39 +0300

    roles controller tests

--- src/roles/tests/roles.controller.spec.ts
+++ src/roles/tests/roles.controller.spec.ts
@@ -1,59 +1,86 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import { User } from "@app/users/entities/user.entity";
+import { TestingModule, Test } from "@nestjs/testing";
 import { CreateRoleDto } from "../dto/create-role.dto";
-import { Role } from "../entities/role.entity";
+import { UpdateRoleDto } from "../dto/update-role.dto";
 import { RolesController } from "../roles.controller";
 import { RolesService } from "../roles.service";
-import { rolesMock } from "./mocks";
+import { RoleViewModel } from "../view-models";
+import { rolesMock } from "@app/common/test/stubs";
+import { QueryParamsDTO } from "@app/common/dto/query-params.dto";
 
-const mockRolesService = {
-    getRoles: jest.fn().mockResolvedValue(rolesMock),
-    createRole: jest.fn((dto: CreateRoleDto) => {
-        const role = new Role();
-        role.name = dto.name;
-        role.id = 1;
-
-        return role;
-    }),
-};
-
-describe("Roles controller", () => {
-    let controller: RolesController;
+describe("RolesController", () => {
+    let rolesController: RolesController;
+    let rolesService: RolesService;
+    let user: User;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [RolesController],
-            providers: [RolesService],
-        })
-            .overrideProvider(RolesService)
-            .useValue(mockRolesService)
-            .compile();
+            providers: [
+                {
+                    provide: RolesService,
+                    useValue: {
+                        createRole: jest.fn().mockResolvedValue(new RoleViewModel()),
+                        getRoles: jest.fn().mockResolvedValue(rolesMock),
+                        getRole: jest.fn().mockResolvedValue(new RoleViewModel()),
+                        updateRole: jest.fn().mockResolvedValue(new RoleViewModel()),
+                        deleteRole: jest.fn().mockResolvedValue(undefined),
+                    },
+                },
+            ],
+        }).compile();
 
-        controller = module.get<RolesController>(RolesController);
+        rolesController = module.get<RolesController>(RolesController);
+        rolesService = module.get<RolesService>(RolesService);
+        user = new User();
     });
 
-    it("RolesService should be defined", () => {
-        expect(controller).toBeDefined();
+    it("should create a role", async () => {
+        const dto = new CreateRoleDto();
+
+        const result = await rolesController.create(dto, user);
+
+        expect(result).toBeInstanceOf(RoleViewModel);
+        expect(rolesService.createRole).toHaveBeenCalledWith(dto, user);
     });
 
-    describe("create a role", () => {
-        it("should create new role", async () => {
-            const createRoleDto: CreateRoleDto = {
-                name: "mockRole",
-            };
+    it("should find all roles", async () => {
+        const queryParams: QueryParamsDTO = {
+            limit: 10,
+            page: 0,
+        };
 
-            expect(controller.create(createRoleDto)).toEqual({
-                id: expect.any(Number),
-                name: createRoleDto.name,
-            });
-            expect(controller.create(createRoleDto)).toBeInstanceOf(Role);
-        });
+        const result = await rolesController.findAll(queryParams);
+
+        expect(Array.isArray(result)).toBe(true);
+        expect(rolesService.getRoles).toHaveBeenCalledWith(queryParams);
     });
 
-    describe("Get roles", () => {
-        it("successfully retrieve roles", async () => {
-            const roles = await controller.getRoles();
+    it("should find a role by id", async () => {
+        const id = 1;
 
-            expect(roles).toEqual(rolesMock);
-        });
+        const result = await rolesController.findOne(id);
+
+        expect(result).toBeInstanceOf(RoleViewModel);
+        expect(rolesService.getRole).toHaveBeenCalledWith(id);
+    });
+
+    it("should update a role", async () => {
+        const id = 1;
+
+        const dto = new UpdateRoleDto();
+        const result = await rolesController.update(id, dto, user);
+
+        expect(result).toBeInstanceOf(RoleViewModel);
+        expect(rolesService.updateRole).toHaveBeenCalledWith(id, dto, user);
+    });
+
+    it("should remove a role", async () => {
+        const id = 1;
+
+        const result = await rolesController.remove(id);
+
+        expect(result).toBeUndefined();
+        expect(rolesService.deleteRole).toHaveBeenCalledWith(id);
     });
 });
