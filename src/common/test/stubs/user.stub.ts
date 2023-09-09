be929c668b65ae26ad0ca1160e5c774045e2ba5a
Vladyslav Bykov <vladyslav.bykov@sombrainc.com>
2023-09-09 08:35:07 +0300

    WIP: Students module

--- src/common/test/stubs/user.stub.ts
+++ src/common/test/stubs/user.stub.ts
@@ -2,6 +2,7 @@ import { User } from "@app/users/entities/user.entity";
 import { adminRoleStub, instructorRoleStub, studentRoleStub, unexistedRoleStub } from "./role.stub";
 import { getRandomNumber } from "../utils";
 import { E2ETestData } from "@app/common/enum";
+import { StudentListViewModel } from "@app/students/view-models";
 
 const adminUserStub: User = new User();
 adminUserStub.id = 1;
@@ -18,6 +19,32 @@ studentUserStub.firstName = "John Student";
 studentUserStub.lastName = "Doe";
 studentUserStub.role = studentRoleStub;
 studentUserStub.group = null;
+
+const studentUserStub2: User = new User();
+studentUserStub2.id = 4;
+studentUserStub2.email = "student@unexisted2.com";
+studentUserStub2.firstName = "John Student2";
+studentUserStub2.lastName = "Doe";
+studentUserStub2.role = studentRoleStub;
+studentUserStub2.group = null;
+
+const studentMockList: User[] = [studentUserStub, studentUserStub2];
+
+const studentVMStub = new StudentListViewModel();
+studentVMStub.id = studentUserStub.id;
+studentVMStub.email = studentUserStub.email;
+studentVMStub.firstName = studentUserStub.firstName;
+studentVMStub.lastName = studentUserStub.lastName;
+studentVMStub.group = null;
+
+const studentVMStub2 = new StudentListViewModel();
+studentVMStub2.id = studentUserStub2.id;
+studentVMStub2.email = studentUserStub2.email;
+studentVMStub2.firstName = studentUserStub2.firstName;
+studentVMStub2.lastName = studentUserStub2.lastName;
+studentVMStub2.group = null;
+
+const studentVMMockList: StudentListViewModel[] = [studentVMStub, studentVMStub2];
 
 const instructorUserStub: User = new User();
 instructorUserStub.id = 3;
@@ -42,4 +69,12 @@ const e2eInstructorStub: Partial<User> = {
     lastName: "Deer",
 };
 
-export { adminUserStub, studentUserStub, instructorUserStub, unexistedUserStub, e2eInstructorStub };
+export {
+    adminUserStub,
+    studentUserStub,
+    instructorUserStub,
+    unexistedUserStub,
+    e2eInstructorStub,
+    studentMockList,
+    studentVMMockList,
+};
