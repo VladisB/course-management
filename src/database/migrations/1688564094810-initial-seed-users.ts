2412faaa3de03a2a6fce02c5c1b2049bcc47e8b8
Vladyslav Bykov <vladyslav.bykov@sombrainc.com>
2023-09-09 15:36:57 +0300

    Migrations update

--- src/database/migrations/1688564094810-initial-seed-users.ts
+++ src/database/migrations/1688564094810-initial-seed-users.ts
@@ -1,7 +1,7 @@
 import { RoleName, PredefinedUser } from "@app/common/enum";
 import { Role } from "@app/roles/entities/role.entity";
-import { User } from "@app/users/entities/user.entity";
 import { MigrationInterface, QueryRunner } from "typeorm";
+import * as bcrypt from "bcryptjs";
 
 // TODO: move to env
 const testPassword = "testsdfsfdDSDD12@";
@@ -9,43 +9,58 @@ const testPassword = "testsdfsfdDSDD12@";
 // PURPOSE: Seed the database with initial data: roles and predefined users
 export class InitialSeed1688564094810 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const users = queryRunner.manager.create(User, [
-            {
-                email: PredefinedUser.Admin,
-                password: testPassword,
-                firstName: "Amanda",
-                lastName: "Rodriguez",
-                role: null,
-            },
-            {
-                email: PredefinedUser.Instructor,
-                password: testPassword,
-                firstName: "Joshua",
-                lastName: "Parker",
-                role: null,
-            },
-            {
-                email: PredefinedUser.Student,
-                password: testPassword,
-                firstName: "Rachel",
-                lastName: "Chen",
-                role: null,
-            },
-        ]);
+        const { salt: sAdmin, password: pAdmin } = await hashPassword(testPassword);
+        const { salt: sInstructor, password: pInstructor } = await hashPassword(testPassword);
+        const { salt: sStudent, password: pStudent } = await hashPassword(testPassword);
 
-        await queryRunner.manager.save(users);
+        await queryRunner.manager
+            .query(
+                `INSERT INTO "user" ("email", "password", "first_name", "last_name", "salt", "role_id") VALUES ($1, $2, $3, $4, $5, $6), ($7, $8, $9, $10, $11, $12), ($13, $14, $15, $16, $17, $18)`,
+                [
+                    PredefinedUser.Admin,
+                    pAdmin,
+                    "Amanda",
+                    "Rodriguez",
+                    sAdmin,
+                    null,
+                    PredefinedUser.Instructor,
+                    pInstructor,
+                    "Joshua",
+                    "Parker",
+                    sInstructor,
+                    null,
+                    PredefinedUser.Student,
+                    pStudent,
+                    "Rachel",
+                    "Chen",
+                    sStudent,
+                    null,
+                ],
+            )
+            .then(() => {
+                console.log("Users created");
+            });
 
-        const admin = await queryRunner.manager.findOne(User, {
-            where: {
-                email: PredefinedUser.Admin,
-            },
-        });
+        const users = await queryRunner.manager.query(`SELECT * FROM "user"`);
 
-        await queryRunner.manager.insert(Role, [
-            { name: RoleName.Admin, createdBy: admin, modifiedBy: admin },
-            { name: RoleName.Student, createdBy: admin, modifiedBy: admin },
-            { name: RoleName.Instructor, createdBy: admin, modifiedBy: admin },
-        ]);
+        const admin = await queryRunner.manager
+            .query(`SELECT * FROM "user" WHERE "email" = $1 LIMIT 1`, [PredefinedUser.Admin])
+            .then((res) => res[0]);
+
+        await queryRunner.manager.query(
+            `INSERT INTO "role" ("name", "created_by", "modified_by") VALUES ($1, $2, $3), ($4, $5, $6), ($7, $8, $9)`,
+            [
+                RoleName.Admin,
+                admin.id,
+                admin.id,
+                RoleName.Student,
+                admin.id,
+                admin.id,
+                RoleName.Instructor,
+                admin.id,
+                admin.id,
+            ],
+        );
 
         const adminRole = await queryRunner.manager.findOne(Role, {
             where: {
@@ -66,30 +81,34 @@ export class InitialSeed1688564094810 implements MigrationInterface {
         });
 
         const adminUser = users.find((user) => user.email === PredefinedUser.Admin);
-        adminUser.role = adminRole;
-        await queryRunner.manager.save(adminUser);
+        await queryRunner.manager.query(`UPDATE "user" SET "role_id" = $1 WHERE "id" = $2`, [
+            adminRole.id,
+            adminUser.id,
+        ]);
 
         const instructorUser = users.find((user) => user.email === PredefinedUser.Instructor);
-        instructorUser.role = instructorRole;
-        await queryRunner.manager.save(instructorUser);
+        await queryRunner.manager.query(`UPDATE "user" SET "role_id" = $1 WHERE "id" = $2`, [
+            instructorRole.id,
+            instructorUser.id,
+        ]);
 
         const studentUser = users.find((user) => user.email === PredefinedUser.Student);
-        studentUser.role = studentRole;
-        await queryRunner.manager.save(studentUser);
+        await queryRunner.manager.query(`UPDATE "user" SET "role_id" = $1 WHERE "id" = $2`, [
+            studentRole.id,
+            studentUser.id,
+        ]);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        let users = await queryRunner.manager.find(User);
-        users = users.map((user) => {
-            user.role = null;
-            return user;
-        });
-
-        await queryRunner.manager.save(users);
-
-        const roles = await queryRunner.manager.find(Role);
-
-        await queryRunner.manager.remove(roles);
-        await queryRunner.manager.remove(users);
+        await queryRunner.manager.query(`UPDATE "user" SET "role_id" = $1`, [null]);
+        await queryRunner.manager.query(`DELETE FROM "role"`);
+        await queryRunner.manager.query(`DELETE FROM "user"`);
     }
+}
+
+async function hashPassword(rawPassword: string): Promise<{ salt: string; password: string }> {
+    const salt = await bcrypt.genSalt();
+    const password = await bcrypt.hash(rawPassword, salt);
+
+    return { salt, password };
 }
